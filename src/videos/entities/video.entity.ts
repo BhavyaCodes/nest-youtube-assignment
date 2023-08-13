c73b38846267c6032cb66dd7fb95f2440ab5aaf7
bhavyaCodes <xbhavyatomar@gmail.com>
2023-08-13 13:20:41 +0530

    create TypeORM video entity

--- src/videos/entities/video.entity.ts
+++ src/videos/entities/video.entity.ts
@@ -1,7 +1,25 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
-
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+// Define an entity (e.g., "Video") to store video details
+//(e.g., "id", "title", "description", "publishedAt", "thumbnailUrl", "videoUrl", etc.).
 @Entity()
 export class Video {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: number;
+
+  @Column({ type: 'varchar', length: 500, nullable: false })
+  title: string;
+
+  @Column({ type: 'varchar', length: 2000, nullable: true })
+  description: string;
+
+  @Column({
+    type: 'timestamptz',
+  })
+  publishedAt: Date;
+
+  @Column({ type: 'varchar', length: 300, nullable: false })
+  thumbnailUrl: string;
+
+  @Column({ type: 'varchar', length: 300, nullable: false })
+  videoUrl: string;
 }
